d61d4d5d93fb1626c4c757b2e265b9295405967c
유정우 <jwyoo@yujeong-uui-MacBook-Pro.local>
2017-06-24 11:15:06 +0900

    Setting Deployable Branch

---
+++ HaveToGoHome/LocalSearchService.swift
@@ -0,0 +1,13 @@
+//
+//  LocalSearchService.swift
+//  HaveToGoHome
+//
+//  Created by 유정우 on 2017. 6. 24..
+//  Copyright © 2017년 prohuhackers. All rights reserved.
+//
+
+import Foundation
+
+class LocalSearchService: NSObject {
+    
+}
